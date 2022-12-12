487af3c68d4f943ad416b12e7717d962a29b276e
SalomanYu <yunoshev.dev@gmail.com>
2022-12-12 23:30:59 +0300

    upgrade spacy method

--- python/pairs/generateSpacyPairs.py
+++ python/pairs/generateSpacyPairs.py
@@ -7,15 +7,27 @@ def match_skills_by_spacy(skills: list[storage.Infinitive], autosave: bool = Fal
     nlp = ru_core_news_lg.load()
     lones: list[str] = []
     pairs: list[storage.Pair] = []
-    for index1 in track(range(len(skills)), description="[yellow]Find pairs by spacy..."):
+    for index1 in track(range(10,len(skills)), description="[yellow]Find pairs by spacy..."):
+        has_pair = False
         infinitive = skills[index1].InfinitiveForm
         normal = skills[index1].NormalForm
         doc1 = nlp(infinitive)
-        try:
-            pair_for_skill = next(skills[index2].NormalForm for index2 in range(index1+1, len(skills)) if doc1.similarity(nlp(skills[index2].InfinitiveForm))*100 > 80)
-            if autosave:storage.save_skills_pairs((storage.Pair(normal, pair_for_skill), ))
-            else: pairs.append(storage.Pair(normal, pair_for_skill))
-        except StopIteration: 
+        if not doc1.vector_norm: continue
+        for index2 in range(index1+1, len(skills)):
+            doc2 = nlp(skills[index2].InfinitiveForm)
+            if not doc2.vector_norm: continue
+            similarity = doc1.similarity(doc2) * 100
+            if similarity >= 95:
+                has_pair = True
+                if autosave:storage.save_skills_lone((normal,)) # Сохраняем только один вариант, т.к понимаем, что у него есть пара, для которой вероятно не требуется подтверждения
+                else: lones.append(normal)
+                break
+            elif similarity >= 80:
+                has_pair = True
+                if autosave:storage.save_skills_pairs((storage.Pair(normal, skills[index2].NormalForm), ))
+                else: pairs.append(storage.Pair(normal, skills[index2].NormalForm))
+                break
+        if not has_pair:
             if autosave:storage.save_skills_lone((normal, ))
             else:lones.append(normal)
     return lones, pairs
